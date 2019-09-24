2d5a2dcaf2cd27194aa4e74af89b73d5be1714ae
kat-star <katherinewkwong@gmail.com>
2019-09-23 19:41:14 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,41 @@
 class Employee
+  attr_reader :name
+  attr_accessor :salary, :manager_name
+  @@all = []
+
+  def initialize(name, salary, manager_name)
+    @name = name
+    @salary = salary
+    @manager_name = manager_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.paid_over(salary)
+    self.all.select { |employee| employee.salary > salary }
+  end
+
+  def self.find_by_department(department)
+    self.all.each do |employee| 
+      if employee.manager_name.department == department
+        return employee
+      end
+    end
+  end
+
+  def tax_bracket
+    my_salary = self.salary
+    my_salary_below = my_salary - 1000
+    my_salary_above = my_salary + 1000
+    
+    Employee.all.select do |employee| 
+      if employee != self
+        employee.salary.between?(my_salary_below, my_salary_above) 
+      end
+    end
+  end
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,34 @@ require 'pry'
 
 #Test your code here
 
+sarah = Manager.new("Sarah", "engineering", 30)
+paul = Manager.new("Paul", "sales", 27)
 
-binding.pry
+sarah.hire_employees("Saul", 50000)
+sarah.hire_employees("Joe", 3000)
+paul.hire_employees("Sal", 2000)
+
+ann = Employee.new("ann", 100000, paul)
+joel = Employee.new("joel", 75000, sarah)
+bob = Employee.new("bob", 65000, sarah)
+lacy = Employee.new("lacy", 100100, paul)
+# ze(name, salary, manager_name)
+
+# puts "list of Sarah's employees"
+# p sarah.employees
+
+# p Employee.find_by_department("sales")
+# puts
+# p Employee.all
+
+p lacy
+puts
+p lacy.tax_bracket
+
+# puts
+# p sarah.employees.map { |employee| employee.salary }
+
+
+
+# binding.pry
 puts "done"

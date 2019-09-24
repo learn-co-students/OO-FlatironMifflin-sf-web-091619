ac1c190c057d28190a7f1d55b796d5d86ee9f197
github username <github email address>
2019-09-24 09:05:02 -0700

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,41 @@
 class Employee
 
+    attr_accessor :name, :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(amount)
+        @@all.map do |employees|
+            employees.salary > amount
+        end
+    end
+
+    def self.find_by_department(department_name)
+        man = Manager.all.find do |manager|
+            manager.department == department_name
+        end
+        man.employees.first
+    end
+
+    def find_by_manager_name
+        @manager.name
+    end
+
+    def tax_bracket
+        @@all.select do |employee|
+            employee.salary.between?(self.salary - 1000, self.salary + 1000)
+        end 
+    end
+
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,13 @@ require 'pry'
 
 #Test your code here
 
+tm = Manager.new("Norris", "Tech", 25)
+tt = Manager.new("Bob", "Tech", 35)
+te = Employee.new("sdfds", 200, tm)
+
+
+tt.hire_employee("sdfds", 200)
+tm.hire_employee("Jdfdsfim", -200)
 
 binding.pry
 puts "done"
